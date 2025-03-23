5982b10573e9dce84f84efe7847facfb75afca4f
bailee1531 <baileesegars2016@gmail.com>
2025-03-23 00:23:01 -0500

    Tested and modified simulator. Changed file to append instead of overwriting after each button press. Changed to writing a fixed amount of data points

--- simulator/simData.py
+++ simulator/simData.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from csv import writer
 import random
 
 def get_sim_data(weather):
@@ -8,26 +9,40 @@ def get_sim_data(weather):
 def sim_temp(weather):
     if weather == 'Rain':
         low, high = 7.77, 18.33
-    if weather == 'Hurricane Event':
+    elif weather == 'Hurricane Event':
         low, high = 26.1, 27.2
-    if weather == 'Tornado Event':
+    elif weather == 'Tornado Event':
         low, high = 18.33, 28.89
+    else:
+        low, high = 7.21, 25.5
     return random.uniform(low, high)
 
 def sim_pressure(weather):
     if weather == 'Rain':
         low, high = 29.80, 30.20
-    if weather == 'Hurricane Event':
+    elif weather == 'Hurricane Event':
         low, high = 26.57, 29.94
-    if weather == 'Tornado Event':
+    elif weather == 'Tornado Event':
         low, high = 23.90, 26.90
+    else:
+        low, high = 29.90, 30.10
     return random.uniform(low, high)
 
 def sim_wind(weather):
     if weather == 'Rain':
         low, high = 2.0, 5.0
-    if weather == 'Hurricane Event':
+    elif weather == 'Hurricane Event':
         low, high = 33.0, 69.0
-    if weather == 'Tornado':
+    elif weather == 'Tornado Event':
         low, high = 49.1, 134.11
+    else:
+        low, high = 3.58, 5.36
     return random.uniform(low, high)
+
+def log_sim_data(weather):
+    with open('simdata.csv', 'a', newline='') as f:
+        dataWriter = writer(f)
+
+        for i in range(0, 50000):
+            data = get_sim_data(weather)
+            dataWriter.writerow(data)
--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,13 +1,16 @@
-from dataLogger import log_sense_data
-from dataLogger import log_sim_data
+from simData import log_sim_data
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Center
 from textual.widgets import Button, Header
-
+from csv import writer
 
 class ButtonsApp(App[str]):
     CSS_PATH = "buttons.tcss"
 
+    with open('simdata.csv', 'w', newline='') as f:
+        dataWriter = writer(f)
+        dataWriter.writerow(['DateTime', 'Temperature', 'Pressure', 'Wind Speed'])    # Sets column headers
+        
     def compose(self) -> ComposeResult:
         yield Header()
         yield Horizontal(
@@ -21,14 +24,13 @@ class ButtonsApp(App[str]):
         self.title = "Weather Data Simulator"
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
-        if str(event.button.name) == 'No Weather Events':
-            log_sense_data(True)
-        elif str(event.button.label) == 'Stop Event':
+        if str(event.button.label) == 'Stop Event':
             event.button.label = event.button.name
-            log_sim_data(False, str(event.button.name))
+            log_sim_data(str(event.button.name))
         else:
             event.button.label = 'Stop Event'
-            log_sim_data(True, str(event.button.name))
+            log_sim_data(str(event.button.name))
 
 if __name__ == "__main__":
-    ButtonsApp().run()
+    app = ButtonsApp()
+    app.run()
