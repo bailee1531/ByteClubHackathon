80c9eef9e80c39b2f002ee618a07b33194e4c370
bailee1531 <baileesegars2016@gmail.com>
2025-03-23 00:49:46 -0500

    Removed extra logging function call. Added comments

--- simulator/simData.py
+++ simulator/simData.py
@@ -2,10 +2,13 @@ from datetime import datetime, timedelta
 from csv import writer
 import random
 
+## Creates a list of the simulated data and returns it to the logging function
 def get_sim_data(weather, time):
     simData = [time, sim_temp(weather), sim_pressure(weather), sim_wind(weather)]
     return simData
 
+## Generates a temperature datapoint that is within the typical range associated
+# with each weather event in Celsius and returns it to be put in data list
 def sim_temp(weather):
     if weather == 'Rain':
         low, high = 7.77, 18.33
@@ -17,6 +20,8 @@ def sim_temp(weather):
         low, high = 7.21, 25.5
     return random.uniform(low, high)
 
+## Generates a pressure datapoint that is within the typical range associated
+# with each weather event in inches of Mercury and returns it to be put in data list
 def sim_pressure(weather):
     if weather == 'Rain':
         low, high = 29.80, 30.20
@@ -28,6 +33,8 @@ def sim_pressure(weather):
         low, high = 29.90, 30.10
     return random.uniform(low, high)
 
+## Generates a wind speed datapoint that is within the typical range associated
+# with each weather event in m/s and returns it to be put in data list
 def sim_wind(weather):
     if weather == 'Rain':
         low, high = 2.0, 5.0
@@ -39,6 +46,9 @@ def sim_wind(weather):
         low, high = 3.58, 5.36
     return random.uniform(low, high)
 
+## Calls get_sim_data in a for loop to generate 50000 lists of datapoints
+# and writes them to the file line by line. Simulates time as if data
+# is collected once per second
 def log_sim_data(weather):
     time = datetime.now()
     with open('simdata.csv', 'a', newline='') as f:
--- simulator/simulator.py
+++ simulator/simulator.py
@@ -4,32 +4,40 @@ from textual.containers import Horizontal, Center
 from textual.widgets import Button, Header
 from csv import writer
 
+## Creates the app class
 class ButtonsApp(App[str]):
-    CSS_PATH = "buttons.tcss"
+    CSS_PATH = "buttons.tcss"   # css file for docking the header and setting margins
 
+    # creates the simulated data csv file
     with open('simdata.csv', 'w', newline='') as f:
         dataWriter = writer(f)
         dataWriter.writerow(['DateTime', 'Temperature', 'Pressure', 'Wind Speed'])    # Sets column headers
-        
+
+    ## Creates the buttons
     def compose(self) -> ComposeResult:
-        yield Header()
+        yield Header()  # calls the docked header
         yield Horizontal(
             Center(Button("No Weather Events", variant='primary', name='No Weather Events')),
             Center(Button.success("Rain", name='Rain')),
             Center(Button.warning("Hurricane Event", name='Hurricane Event')),
             Center(Button.error("Tornado Event", name='Tornado Event'))
-        )
+        )   # buttons are centered and in a horizontal line
     
+    ## Sets the title written in the header
     def on_mount(self) -> None:
         self.title = "Weather Data Simulator"
 
+    ## Event listener for button presses
     def on_button_pressed(self, event: Button.Pressed) -> None:
+        # if the label is 'stop event', then the button has already been pressed
+        # if the button has already been pressed, then the script to log simulated data has already run
         if str(event.button.label) == 'Stop Event':
             event.button.label = event.button.name
-            log_sim_data(str(event.button.name))
+        # if the label is anything else, then it is the name of the simulated weather event
+        # if the label shows a simulated weather event, then it needs to log simulated data
         else:
-            event.button.label = 'Stop Event'
-            log_sim_data(str(event.button.name))
+            event.button.label = 'Stop Event'   # change the label
+            log_sim_data(str(event.button.name))    # passes the name because it does not change
 
 if __name__ == "__main__":
     app = ButtonsApp()
